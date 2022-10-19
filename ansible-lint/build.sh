df0465d870e05d53cd6e780938854eda3bb2d575
Marijn Koesen <github@koesen.nl>
2022-10-19 15:27:23 +0200

    Update ansible-line image

--- ansible-lint/build.sh
+++ ansible-lint/build.sh
@@ -1,5 +1,5 @@
-#docker build -t marijnkoesen/ansible-lint:v3.4.21 -f Dockerfile .
-docker push marijnkoesen/ansible-lint:v3.4.21
+docker build -t marijnkoesen/ansible-lint:v4.1.0 -f Dockerfile .
+docker push marijnkoesen/ansible-lint:v4.1.0
 
-docker tag marijnkoesen/ansible-lint:v3.4.21 marijnkoesen/ansible-lint:latest
+docker tag marijnkoesen/ansible-lint:v4.1.0 marijnkoesen/ansible-lint:latest
 docker push marijnkoesen/ansible-lint:latest
